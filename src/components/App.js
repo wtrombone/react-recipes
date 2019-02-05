73e704bd965c6a4a60772c5257d636af7ca38bc1
wtrombone <w.trombone@gmail.com>
2019-02-05 03:36:51 -0500

    Add NotFound route fallback

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import NotFound from './NotFound';
 import Favorites from './Favorites';
 
 const App = () => (
   <BrowserRouter>
     <main>
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/favorites" component={Favorites} />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/favorites" component={Favorites} />
+        <Route component={NotFound} />
+      </Switch>
     </main>
   </BrowserRouter>
 );
